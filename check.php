016bc6a7fda8010818f0e325baee117ec025764d
tadakatu <w.shino229@gmail.com>
2021-03-12 12:05:49 +0900

    css　名前変更

--- check.php
+++ check.php
@@ -14,7 +14,7 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/watanabe.css" />
         <title>購入確認</title>
     </head>
 
